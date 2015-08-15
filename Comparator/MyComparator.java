a472ac46f45c153843313f4b29add44a824cb55a
kamaleshwar <kp2601@rit.edu>
2015-08-15 14:25:03 -0400

    Create MyComparator.java

---
+++ Comparator/MyComparator.java
@@ -0,0 +1,16 @@
+/*
+ *MyComparator.java
+ *
+ * Version:
+ *     $Id: MyComparator.java,v 1.7 10/13/2014 $
+ *     
+ * @Authors: Raj & Kamaleshwar  
+ *
+ * Revisions:
+ *     $Log$
+ */
+
+public interface MyComparator<T> {
+
+	int compare(T o1,T o2);
+}
